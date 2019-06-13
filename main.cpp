2cf5bbd4cac39760d642cfd6c627d5f9bf660725
CoseCoder <838491325@qq.com>
2019-06-13 21:35:25 +0800

    修改界面

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+	int cx = GetSystemMetrics(SM_CXFULLSCREEN);
+	int cy = GetSystemMetrics(SM_CYFULLSCREEN);
+
     MainWindow w;
-    w.show();
+	w.setFixedSize(cx, cy);
+	w.showMaximized();
 
     return a.exec();
 }
